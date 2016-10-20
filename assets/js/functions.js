f220c39622aa8cffffedc99762dbde4e1f5b52c7
Fabien <fabien.webdev@gmail.com>
2016-10-20 12:14:11 +0900

    Finished the weather script

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,29 +2,102 @@ $(document).ready(function() {
    // Stuff to do as soon as the DOM is ready
   geoWeather();
 
-   function geoWeather() {
+  function geoWeather() {
+
+    // Declare all the element variables
+    var msgEl = $('#customMsg');
+    var town = $('.town');
+    var country = $('#country');
+    var weather = $('#weather');
+    var icon = $('#icon');
+    var description = $('#description');
+    var temperature = $('#temperature');
+
    // Get location of the user
-   if (Modernizr.geolocation) {
-     $('.msg').text('Checking your location...');
-     navigator.geolocation.getCurrentPosition(success, fail);
-   } else {
-     console.log("Couldn't find your location, make sure to enable JavaScript and accept the request to locate you.");
-   }
+    // Check if the user has geolocation enabled
+    if (Modernizr.geolocation) {
+      msgEl.text('Checking your location...');
+      navigator.geolocation.getCurrentPosition(success, fail);
+    } else {
+      msgEl.html("<span class=\"error\">Couldn't find your location, make sure to accept the request to locate you.</span>");
+    }
 
-   // Make the API call
+    // If geolocation is enabled & geodata retrieved
+    function success(position){
+      msgEl.fadeOut(500);
 
+      // Store the geodata into variables
+      var longitude = position.coords.longitude;
+      var latitude = position.coords.latitude;
 
-   // Update content
+      // Make the API call
 
+      ajaxReq('imperial'); // Default call when user load the page (with imperial units by default)
 
-  }
+      function ajaxReq(unit) {
+        var unit = unit; // Get the unit of the request
+        // Build the request url with custom longitude/latitude and units
+        var urlQuery = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=' + unit + '&appid=35187c9e07d9d7aa6b37ca9f15703bc5';
 
+        $.ajax({
+          type: "GET",
+          url: urlQuery, //Pass the custom url
+          timeout: 2000,
+          beforeSend: function(){
+            msgEl.text('Loading data...'); // Display a message while its loading
+          },
+          complete: function(){
+            msgEl.text(''); // Delete the loading message
+          },
+          success: function(data){
+            console.log(data);
+            // Update content
+            town.text(data.name);
+            country.text(', ' + data.sys.country);
+            weather.text(data.weather[0].main);
+            icon.html('<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="' + data.weather[0].main + ' icon"</img>');
+            description.text(data.weather[0].description);
 
-});
+              // Insert the right unit symbol after the temperature
+            if (unit === 'metric') {
+              temperature.text(data.main.temp + '°C');
+            } else if (unit === 'imperial') {
+              temperature.text(data.main.temp + '°F');
+            } else {
+              temperature.text(data.main.temp + 'K');
+            }
+          },
+          error: function(data){
+            msgEl.html("<span class=\"error\">An error occured while retrieving data. Please try again later.</span>"); // Display an error message if the ajax request failed
+          }
 
-/*
+        });  // end of ajax()
 
+      };  // end of ajaxReq()
 
-http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=35187c9e07d9d7aa6b37ca9f15703bc5
-http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=imperial&appid=35187c9e07d9d7aa6b37ca9f15703bc5
-*/
+      // Button script
+      var convert = $('a#convert'); // Get the button
+      convert.on('click', function(e){ // On click
+        e.preventDefault(); // Prevent default behavior
+        if(convert.text() === 'To Celsius'){ // Change the text and call ajaxReq to get the corresponding unit data
+          convert.text('To Fahrenheit');
+          ajaxReq('metric');
+        } else if (convert.text() === 'To Fahrenheit') {
+          convert.text('To Celsius');
+          ajaxReq('imperial');
+        }
+      })
+
+    }  // end of success()
+
+    // If geolocation is enabled & geodata failed
+    function fail(msg){
+      msgEl.html("<span class=\"error\">Couldn't find your location, make sure to accept the request to locate you.</span>");
+      // More precise message for debug purpose
+      console.log(msg);
+      console.log('error code: ' + msg.code + ' / error message: ' + msg.message);
+    }
+
+  }  // end of geoWeather()
+
+});  // end of ready()
